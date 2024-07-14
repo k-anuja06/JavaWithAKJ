3bdee64e3af77e6a4967e61070dab45469a570cc
Anuja Kumari <166990955+k-anuja06@users.noreply.github.com>
2024-07-14 19:14:10 +0000

    fix error for floating avg

--- A02AvgOfTwoNumber.java
+++ A02AvgOfTwoNumber.java
@@ -1,8 +1,8 @@
 public class A02AvgOfTwoNumber {
     public static void main(String[] args) {
-        int num1 = 10;
+        int num1 = 15;
         int num2 = 20;
-        int avg = (num1 + num2) /2;
+        float avg = (num1 + num2) /2f;
         System.out.println("The avg of " + num1+ " and "+num2+": " +avg );
     }
 }
